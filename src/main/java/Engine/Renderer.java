cd899d9dec3c96455167ea9611ebd656de3a50c3
Larqqa <32098438+Larqqa@users.noreply.github.com>
2020-09-10 22:03:54 +0300

    saving some geometric changes

--- src/main/java/Engine/Renderer.java
+++ src/main/java/Engine/Renderer.java
@@ -47,6 +47,56 @@ public class Renderer {
         }
     }
 
+    private float sign (int [] one, int [] two, int [] three)
+    {
+        return (one[0] - three[0]) * (two[1] - three[1]) - (two[0] - three[0]) * (one[1] - three[1]);
+    }
+
+    public void draw2DFillTriangle(int [] one, int [] two, int [] three, int color) {
+        for (int y = 0; y < getPHeight(); y++)  {
+            for (int x = 0; x < getPWidth(); x++) {
+                float d1, d2, d3;
+                boolean has_neg, has_pos;
+
+                int [] point = {x, y};
+
+                d1 = sign(point, one, two);
+                d2 = sign(point, two, three);
+                d3 = sign(point, three, one);
+
+                has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+                has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+                if (!(has_neg && has_pos)) {
+                    setPixel(point[0], point[1], color);
+                }
+            }
+        }
+    }
+
+
+    private boolean pointOnLine(int [] a, int [] b, int [] p) {
+        int ab = (int) Math.sqrt(Math.pow(b[0] - a[0], 2) + Math.pow(b[1] - a[1], 2));
+        int ap = (int) Math.sqrt(Math.pow(p[0] - a[0], 2) + Math.pow(p[1] - a[1], 2));
+        int pb = (int) Math.sqrt(Math.pow(b[0] - p[0], 2) + Math.pow(b[1] - p[1], 2));
+
+        return (ap + pb == ab);
+    }
+
+    public void draw2DTriangle(int [] one, int [] two, int [] three, int color) {
+        int [] point = {0, 0};
+        for (int y = 0; y < getPHeight(); y++)  {
+            for (int x = 0; x < getPWidth(); x++) {
+                point[0] = x;
+                point[1] = y;
+
+                if (pointOnLine(one, two, point)) setPixel(x, y, color);
+                if (pointOnLine(two, three, point)) setPixel(x, y, color);
+                if (pointOnLine(three, one, point)) setPixel(x, y, color);
+            }
+        }
+    }
+
     public void draw2DCircle(int x, int y, int r, int color) {
         int r2 = r * r;
 
@@ -58,8 +108,10 @@ public class Renderer {
             for (int i = x - r; i <= x + r; i++) {
                 int x2 = (int)Math.pow(i - x, 2);
 
+                // System.out.println(r2);
+
                 // test if in-circle
-                if (x2 + y2 <= r2) {
+                if ( (x2 + y2 <= r2) && (x2 + y2 >= r2 - (r * 2)) ) {
                     setPixel(i, j, color);
                 }
             }
--- src/main/java/Pacman/Pacman.java
+++ src/main/java/Pacman/Pacman.java
@@ -11,7 +11,7 @@ public class Pacman extends Game {
         GameSettings settings = new GameSettings();
         settings.setWidth(500);
         settings.setTitle("Pacman");
-        settings.setFpscap(1.0 / 100.0);
+        settings.setFpscap(1.0 / 10.0);
 
         Engine engine = new Engine(this, settings);
         engine.start();
@@ -25,9 +25,17 @@ public class Pacman extends Game {
 
     @Override
     public void render() {
-        renderer.draw2DCircle(mx,my,25,0xffffffff);
-        // renderer.draw2DFillRect(50,50,50,50,0x66666666);
-        // renderer.draw2DRect(mx, my,50,50,0xffffffff);
+        //renderer.draw2DCircle(mx,my,50,0xffffffff);
+        //renderer.draw2DFillCircle(300,100,25,0xffffffff);
+        //renderer.draw2DFillRect(50,50,50,70,0x66666666);
+        //renderer.draw2DRect(mx, my,50,50,0xffffffff);
+
+        int[] one = {10, 10};
+        int[] two = {100, 20};
+        int[] three = {50, 50};
+
+        // renderer.draw2DFillTriangle(one, two, three, 0xffffffff);
+        renderer.draw2DTriangle(one, two, three, 0xffffffff);
     }
 
     public static void main(String[] args) {
